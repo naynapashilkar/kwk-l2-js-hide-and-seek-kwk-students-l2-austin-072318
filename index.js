dacc3e53ac299b4155170060450aeb05b018208c
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-27 18:29:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget () {
+  return document.getElementById("nested")
+}
+
+function increaseRankBy (n) {
+  let lis =  document.getElementById("ul.ranked-list li")
+  
+  for (let i = 1; i < lis.length; i++) {
+  lis[i].innerHTML = (i + n).toString()
+  }
+  console.log("lis")
+}
+
+function deepestChild() {
+  return document.querySelector("")
+}
